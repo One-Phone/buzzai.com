005f69a7595766a403293420c996be7938dcb34a
Quang Nguyen <quangnguyen.devs@gmail.com>
2025-01-01 13:13:21 +0700

    reset pages

---
+++ public/js/blocks/faq.js
@@ -0,0 +1,21 @@
+(self.webpackChunkskeleton_theme = self.webpackChunkskeleton_theme || []).push([[5321], {
+    9863: () => {
+        window.addEventListener("load", (function() {
+            var o = document.querySelectorAll(".faq");
+            null != o && o.length && o.forEach((function(o) {
+                e(o)
+            }
+            ))
+        }
+        ));
+        var e = function(e) {
+            e.style.setProperty("--section-bg-color", window.getComputedStyle(e).backgroundColor),
+            e.style.setProperty("--section-font-color", window.getComputedStyle(e).color)
+        }
+    }
+}, e => {
+    var o;
+    o = 9863,
+    e(e.s = o)
+}
+]);
---
+++ src/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+    urls: {
+      origin: '',
+      api: 'https://api.hushed.com',
+      app: 'https://app.hushed.com',
+      social: {
+        instagram: 'https://www.instagram.com/hushedapp/',
+        youtube: 'https://www.youtube.com/@hushedapp',
+        twitter: 'https://x.com/hushedapp',
+        facebook: 'https://www.facebook.com/hushedapp',
+        linkedin: 'https://www.linkedin.com/company/hushedapp'
+      }
+    }
+  } as const;
