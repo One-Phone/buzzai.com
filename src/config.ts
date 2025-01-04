6890b75839a81e6751daffb17e570e49072ec42e
Quang Nguyen <quangnguyen.devs@gmail.com>
2025-01-04 16:27:58 +0700

    Replace buzz

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,13 @@ export const config = {
     urls: {
       origin: '',
       api: 'https://api.hushed.com',
-      app: 'https://app.hushed.com',
+      app: 'https://app.buzznum.com',
       social: {
-        instagram: 'https://www.instagram.com/hushedapp/',
-        youtube: 'https://www.youtube.com/@hushedapp',
-        twitter: 'https://x.com/hushedapp',
-        facebook: 'https://www.facebook.com/hushedapp',
-        linkedin: 'https://www.linkedin.com/company/hushedapp'
+        instagram: 'https://www.instagram.com/buzznum/',
+        youtube: 'https://www.youtube.com/@buzznum',
+        twitter: 'https://x.com/buzznum',
+        facebook: 'https://www.facebook.com/buzznum',
+        linkedin: 'https://www.linkedin.com/company/buzznum'
       }
     }
   } as const;
